016c56e2c76a482e934ed2c76a9e24d5a76a5a36
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-02-17 19:50:43 -0800

    add onClick to li

---
+++ src/components/navMenu/NavMenu.jsx
@@ -0,0 +1,29 @@
+import './navmenu.scss';
+
+import React from 'react'
+
+export default function NavMenu({ navOpen, toggleNav}) {
+  return (
+    <div className={"navMenu " + (navOpen && "active")}>
+      <ul>
+      <li onClick={toggleNav}>
+          <a href="#welcome">Home</a>
+        </li>
+        <li onClick={toggleNav}>
+          <a href="#about">About</a>
+        </li>
+        <li onClick={toggleNav}>
+          <a href="#projects">Projects</a>
+        </li>
+        <li onClick={toggleNav}>
+          <a href="#contact">Contact</a>
+        </li>
+        <li onClick={toggleNav}>
+          <a href="https://www.canva.com/design/DAEwBE504Vk/xNEYzY-ZtLBoGtA3jAhUVw/view?utm_content=DAEwBE504Vk&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton" target="_blank" rel="noopener noreferrer">Resume</a>
+        </li>
+      </ul>
+
+
+    </div>
+  )
+}
