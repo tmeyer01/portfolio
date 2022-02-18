bd147f53688735f9104232ce6d4c5f07de7ed25c
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-02-17 18:28:16 -0800

    add hamburger and styled

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,15 @@
+import "./nav.scss"
+
+export default function Nav() {
+  return (
+    <div className="nav">
+      <div className="wrapper">
+        <div className="hamburger">
+          <span className='line1'></span>
+          <span className='line2'></span>
+          <span className='line3'></span>
+        </div>
+      </div>
+    </div>
+  )
+}
