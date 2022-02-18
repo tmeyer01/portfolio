58520054a584a6e3e7e6a4a1ddba73d8063fbc4d
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-02-17 18:11:27 -0800

    added welcome message  to component

---
+++ src/components/welcome/Welcome.jsx
@@ -0,0 +1,17 @@
+
+
+export default function Welcome() {
+  return (
+   <div className="welcome">
+
+    <div className="name--title--wrapper">
+      <p>Hello world.</p>
+      <h1>I'm Thomas Meyer,</h1>
+      <h3 className="title"> Full stack web developer</h3>
+
+    </div>
+
+
+   </div>
+  )
+}
