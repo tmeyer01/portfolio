d4a37f9210da81a9095cf46269dc9174c9bd0f8b
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-02-17 18:09:51 -0800

    Changed App to .jsx file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Welcome from "./components/welcome/Welcome";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="sections">
+        <Welcome />
+      </div>
+    </div>
+  );
+}
+
+export default App;
