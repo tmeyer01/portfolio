351cbcf717cab0b441ba4f69b3c12650e6f4e6dc
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-02-18 09:28:07 -0800

    linting welcome component

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,17 +1,15 @@
-import "./welcome.scss"
+import "./welcome.scss";
 
 export default function Welcome() {
   return (
-   <div className="welcome" id="welcome">
-
-    <div className="name--title--wrapper">
-      <p>Hello world <span className="blink">.</span></p>
-      <h1>I'm Thomas Meyer,</h1>
-      <h3 className="title"> Full stack web developer</h3>
-
+    <div className="welcome" id="welcome">
+      <div className="name--title--wrapper">
+        <p>
+          Hello world <span className="blink">.</span>
+        </p>
+        <h1>I'm Thomas Meyer,</h1>
+        <h3 className="title"> Full stack web developer</h3>
+      </div>
     </div>
-
-
-   </div>
-  )
+  );
 }
