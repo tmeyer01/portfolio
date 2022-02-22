aa3ea42be152cd291e4cd786506a9c789563b2c1
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-02-22 11:11:28 -0800

    linting

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -4,21 +4,17 @@ export default function Welcome() {
   return (
     <div className="welcome" id="welcome">
       <div className="container">
+        <div className="name--title--wrapper">
+          <p>
+            Hello world <span className="blink">.</span>
+          </p>
+          <h1>I'm Thomas Meyer,</h1>
+          <h3 className="title"> Full stack web developer</h3>
+        </div>
 
-
-      <div className="name--title--wrapper">
-        <p>
-          Hello world <span className="blink">.</span>
-        </p>
-        <h1>I'm Thomas Meyer,</h1>
-        <h3 className="title"> Full stack web developer</h3>
-      </div>
-
-      <a href="#about" className="downArrow">
+        <a href="#about" className="downArrow">
           <img src="/images/icons/colorarrow.png" alt="icondown" />
-      </a>
-
-
+        </a>
       </div>
     </div>
   );
